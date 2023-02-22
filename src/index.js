4f71bf807ac48b35e4bed8e55a83b4f904eb74cc
Kaspars Jaudzems <kasparsj@gmail.com>
2023-02-22 08:16:57 +0100

    order!

--- src/index.js
+++ src/index.js
@@ -3,65 +3,186 @@ import * as FXRand from 'fxhash_lib/random.js'
 import * as core from "fxhash_lib/core";
 import * as dev from "fxhash_lib/dev";
 import * as mats from "fxhash_lib/materials";
-import {RenderPingPong} from "fxhash_lib/RenderPingPong";
 import {FluidLayer} from "./FluidLayer";
-import {generateHSLPalette, hsl2Color, generateColor} from "../../fxhash_lib/color";
+import {generateColor} from "../../fxhash_lib/color";
 import {FluidStroke} from "./FluidStroke";
 import {devMode, settings, options, layerOptions, lightOptions, effects, chooseComposition, choosePalette} from "./config"
 import {createGUI, createLayerGUI} from "./gui";
+import {renderer, scene, cam} from "fxhash_lib/core";
+import {initVars, palette, hslPalette, colors, comp, layers, strokesPerLayer, histPingPong, histMesh, labels, features} from "./vars";
 
-if (devMode) {
-  dev.initGui(name);
-  createGUI(dev.gui);
+const sketch = {};
+setup();
+
+function setup() {
+  if (devMode) {
+    dev.initGui(settings.name);
+    createGUI(dev.gui);
+  }
+
+  initVars();
+
+  core.init(Object.assign({}, settings, {
+    alpha: comp === 'cells',
+  }));
+  //const {camLight, sunLight, ambLight} = core.initLights(lightOptions);
+  core.initCSS2DRenderer();
+
+  if (devMode) {
+    //core.initControls(cam);
+    dev.initHelpers();
+    //dev.initLighting(lightOptions);
+    dev.initEffects(effects);
+    dev.hideGuiSaveRow();
+  }
+
+  // cam.position.x = 1024;
+  // cam.position.y = 512;
+  // cam.position.z = 1024;
+  // core.lookAt(new THREE.Vector3(0, 0, 0));
+
+  if (comp !== 'cells') {
+    scene.background = colors[0];
+  }
+
+  //scene.add(new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100), new THREE.MeshBasicMaterial({color: new THREE.Color(1, 0, 0)})));
+
+  for (let i=0; i<features.layers; i++) {
+    addLayer(strokesPerLayer);
+  }
+  switch (comp) {
+    case 'cells':
+      createCell();
+      break;
+  }
+
+  core.useEffects(effects);
+  core.animate();
+
+  addEventListeners();
+
+  fxpreview();
 }
 
-const composition = chooseComposition();
-const {cam, scene, renderer} = core.init(Object.assign({}, settings, {
-  alpha: composition === 'cells',
-}));
-//const {camLight, sunLight, ambLight} = core.initLights(lightOptions);
-const labelRenderer = core.initCSS2DRenderer();
-const layers = [];
-const strokesPerLayer = FXRand.int(options.minStrokes, options.maxStrokes);
-const histPingPong = new RenderPingPong(core.width, core.height, {
-  minFilter: THREE.LinearFilter,
-  magFilter: THREE.LinearFilter,
-  depthBuffer: false,
-  stencilBuffer: false,
-  generateMipmaps: false,
-});
-let histMesh;
-const labels = new THREE.Group();
-
-if (devMode) {
-  //core.initControls(cam);
-  dev.initHelpers();
-  //dev.initLighting(lightOptions);
-  dev.initEffects(effects);
-  dev.hideGuiSaveRow();
+function addLayer(numStrokes) {
+  const i = layers.length;
+  layerOptions.push(generateOptions(i));
+  if (devMode) {
+    createLayerGUI(dev.gui, layers, i);
+  }
+  const layer = createLayer(numStrokes);
+  createStrokes(layer, i);
 }
 
-// cam.position.x = 1024;
-// cam.position.y = 512;
-// cam.position.z = 1024;
-// core.lookAt(new THREE.Vector3(0, 0, 0));
-
-// Feature generation
-let features = {
-  palette: choosePalette(),
-  layers: FXRand.int(options.minLayers, options.maxLayers),
-  colorW: FXRand.exp(0.1, 8),
+function createLayer(numStrokes) {
+  const i = layers.length;
+  layers[i] = new FluidLayer(Object.assign({}, layerOptions[i], {
+    numStrokes,
+    maxIterations: options.maxIterations,
+    bgColor: colors[0],
+    transparent: comp === 'cells',
+  }));
+  setLayerColor(layers[i], colors[1]);
+  layers[i].resize(core.width, core.height, 1);
+  const mesh = layers[i].initMesh();
+  scene.add(mesh);
+  return layers[i];
 }
 
-window.$fxhashFeatures = features;
-
-const hslPalette = generateHSLPalette(features.palette);
-const colors = hslPalette.map(hsl2Color);
-if (composition !== 'cells') {
-  scene.background = colors[0];
+function createStrokes(layer, i) {
+  const numStrokes = layer.options.numStrokes;
+  for (let j=0; j<numStrokes; j++) {
+    let stroke;
+    if (i === 0 || options.strokesRel === 'random') {
+      // first layer all strokes are random
+      const speed = FXRand.num(options.minSpeed, options.maxSpeed) * options.speedMult;
+      const isDown = FXRand.bool();
+      stroke = new FluidStroke(FXRand.num(), FXRand.num());
+      stroke.speed = speed;
+      stroke.isDown = isDown;
+      stroke.target.set(FXRand.num(), FXRand.num());
+    }
+    else {
+      let sr = options.strokesRel;
+      if (sr === 'mirrorRand') {
+        sr = FXRand.choice(['mirror', 'mirrorX', 'mirrorY']);
+      }
+      switch (sr) {
+        case 'same':
+          stroke = layers[0].strokes[j].clone();
+          break;
+        case 'mirror':
+        case 'mirrorX':
+        case 'mirrorY':
+        default:
+          stroke = layers[0].strokes[j].clone()[sr || 'mirror']();
+          break;
+      }
+    }
+    layer.addStroke(stroke);
+  }
 }
 
-const validateOptions = (options, i) => {
+function renderToHist() {
+  histPingPong.render(renderer, scene, cam);
+  histPingPong.swap();
+
+  if (histMesh) {
+    histMesh.material.uniforms.tMap.value = histPingPong.texture;
+    histMesh.material.needsUpdate = true;
+  }
+  else {
+    histMesh.material = mats.fullScreenMap({
+      blending: THREE.CustomBlending,
+      transparent: true,
+    }, {
+      map: histPingPong.texture,
+    });
+    scene.add(histMesh);
+  }
+}
+
+function resetLayer(layer) {
+  layer.initRenderer();
+  regenerateLayer(layer);
+  for (let j=0; j<layer.strokes.length; j++) {
+    const speed = FXRand.num(options.minSpeed, options.maxSpeed) * options.speedMult;
+    layer.strokes[j].speed = speed;
+    layer.strokes[j].reset();
+  }
+  const color = generateColor(palette, hslPalette[0]);
+  setLayerColor(layer, color);
+}
+
+function setLayerColor(layer, color) {
+  layer.color = new THREE.Vector4(color.r*256, color.g*256, color.b*256, features.colorW);
+}
+
+function regenerateLayer(layer) {
+  const i = layers.indexOf(layer);
+  Object.assign(layerOptions[i], generateOptions(i));
+  layer.setOptions(layerOptions[i]);
+}
+
+function generateOptions(i) {
+  const minDt = [0.25, 0.25, 0.4, 0.1, 0.3, 0.1];
+  let opts;
+  do {
+    const blendModePass = FXRand.int(0, i > 0 ? 4 : 5);
+    const blendModeView = comp === 'cells' ? 2 : FXRand.int(2, blendModePass === 3 ? 3 : 5);
+    opts = {
+      blendModePass,
+      blendModeView,
+      dt: FXRand.num(minDt[blendModePass], 1.0),
+      K: FXRand.num(0.2, 0.7),
+      nu: FXRand.num(0.4, 0.6),
+      kappa: FXRand.num(0.1, 0.9),
+    };
+  } while (!validateOptions(opts, i));
+  return opts;
+}
+
+function validateOptions(options, i) {
   return true;
   const invalidBlends = ['4-4'];
   const blendModeString = options.blendModePass+'-'+options.blendModeView;
@@ -71,7 +192,7 @@ const validateOptions = (options, i) => {
   if (i > 0 && [2, 4].indexOf(options.blendModeView) > -1 && options.blendModeView === layerOptions[i-1].blendModeView) {
     return false;
   }
-  if (features.palette === 'Black&White') {
+  if (palette === 'Black&White') {
     const invalidBWBlends = ['1-2'];
     if (invalidBWBlends.indexOf(blendModeString) > -1) {
       return false;
@@ -111,7 +232,7 @@ const validateOptions = (options, i) => {
       return false;
     }
   }
-  else if (features.palette === 'Mono') {
+  else if (palette === 'Mono') {
     if (i > 0 && ['2-3', '2-5'].indexOf(blendModeString) > -1) {
       return false;
     }
@@ -119,7 +240,7 @@ const validateOptions = (options, i) => {
       return false;
     }
   }
-  else if (features.palette === 'Analogous') {
+  else if (palette === 'Analogous') {
     if (['2-5'].indexOf(blendModeString) > -1) {
       return false;
     }
@@ -163,25 +284,30 @@ const validateOptions = (options, i) => {
   return true;
 }
 
-const generateOptions = (i) => {
-  const minDt = [0.25, 0.25, 0.4, 0.1, 0.3, 0.1];
-  let opts;
-  do {
-    const blendModePass = FXRand.int(0, i > 0 ? 4 : 5);
-    const blendModeView = composition === 'cells' ? 2 : FXRand.int(2, blendModePass === 3 ? 3 : 5);
-    opts = {
-      blendModePass,
-      blendModeView,
-      dt: FXRand.num(minDt[blendModePass], 1.0),
-      K: FXRand.num(0.2, 0.7),
-      nu: FXRand.num(0.4, 0.6),
-      kappa: FXRand.num(0.1, 0.9),
-    };
-  } while (!validateOptions(opts, i));
-  return opts;
+let numCells = 0;
+const doCreateCell = () => {
+  numCells++;
+  renderToHist();
+  layers.map((layer) => {
+    regenerateLayer(layer);
+  });
 }
 
-const renderFrame = (event) => {
+let timeoutID = -1;
+function createCell() {
+  if (timeoutID > 0) {
+    clearTimeout(timeoutID);
+    doCreateCell();
+  }
+  if (numCells < options.maxCells) {
+    timeoutID = setTimeout(createCell, FXRand.int(500, 7000));
+  }
+  else {
+    // todo: add diagonal animation effect
+  }
+}
+
+function draw(event) {
   core.update();
   dev.update();
   for (let i=0; i<layers.length; i++) {
@@ -192,138 +318,8 @@ const renderFrame = (event) => {
   core.render();
 }
 
-const onKeyDown = (event) => {
-  if (devMode) {
-    dev.keyDown(event, settings, lightOptions, effects);
-  }
-}
-
-const onResize = (event) => {
-  core.resize(window.innerWidth, window.innerHeight);
-  for (let i=0; i<layers.length; i++) {
-    layers[i].resize(window.innerWidth, window.innerHeight, 1);
-  }
-}
-
-const onDblClick = (event) => {
-  if (devMode && !dev.isGui(event.target)) {
-    document.location.reload();
-  }
-}
-
-const addEventListeners = () => {
-  document.addEventListener("keydown", onKeyDown, false);
-  window.addEventListener("resize", onResize);
-  window.addEventListener('renderFrame', renderFrame);
-  renderer.domElement.addEventListener('click', onClick);
-  if (devMode) {
-    window.addEventListener("dblclick", onDblClick);
-  }
-}
-
-const createLayer = (numStrokes) => {
-  const i = layers.length;
-  layers[i] = new FluidLayer(Object.assign({}, layerOptions[i], {
-    numStrokes,
-    maxIterations: options.maxIterations,
-    bgColor: colors[0],
-    transparent: composition === 'cells',
-  }));
-  setLayerColor(layers[i], colors[1]);
-  layers[i].resize(core.width, core.height, 1);
-  const mesh = layers[i].initMesh();
-  scene.add(mesh);
-  return layers[i];
-}
-
-const createStrokes = (layer, i) => {
-  const numStrokes = layer.options.numStrokes;
-  for (let j=0; j<numStrokes; j++) {
-    let stroke;
-    if (i === 0 || options.strokesRel === 'random') {
-      // first layer all strokes are random
-      const speed = FXRand.num(options.minSpeed, options.maxSpeed) * options.speedMult;
-      const isDown = FXRand.bool();
-      stroke = new FluidStroke(FXRand.num(), FXRand.num());
-      stroke.speed = speed;
-      stroke.isDown = isDown;
-      stroke.target.set(FXRand.num(), FXRand.num());
-    }
-    else {
-      let sr = options.strokesRel;
-      if (sr === 'mirrorRand') {
-        sr = FXRand.choice(['mirror', 'mirrorX', 'mirrorY']);
-      }
-      switch (sr) {
-        case 'same':
-          stroke = layers[0].strokes[j].clone();
-          break;
-        case 'mirror':
-        case 'mirrorX':
-        case 'mirrorY':
-        default:
-          stroke = layers[0].strokes[j].clone()[sr || 'mirror']();
-          break;
-      }
-    }
-    layer.addStroke(stroke);
-  }
-}
-
-const renderToHist = () => {
-  histPingPong.render(renderer, scene, cam);
-  histPingPong.swap();
-
-  if (histMesh) {
-    histMesh.material.uniforms.tMap.value = histPingPong.texture;
-    histMesh.material.needsUpdate = true;
-  }
-  else {
-    histMesh = FluidLayer.createMesh();
-    histMesh.material = mats.fullScreenMap({
-      blending: THREE.CustomBlending,
-      transparent: true,
-    }, {
-      map: histPingPong.texture,
-    });
-    scene.add(histMesh);
-  }
-}
-
-const resetLayer = (layer) => {
-  layer.initRenderer();
-  regenerateLayer(layer);
-  for (let j=0; j<layer.strokes.length; j++) {
-    const speed = FXRand.num(options.minSpeed, options.maxSpeed) * options.speedMult;
-    layer.strokes[j].speed = speed;
-    layer.strokes[j].reset();
-  }
-  const color = generateColor(options.palette, hslPalette[0]);
-  setLayerColor(layer, color);
-}
-
-const setLayerColor = (layer, color) => {
-  layer.color = new THREE.Vector4(color.r*256, color.g*256, color.b*256, features.colorW);
-}
-
-const regenerateLayer = (layer) => {
-  const i = layers.indexOf(layer);
-  Object.assign(layerOptions[i], generateOptions(i));
-  layer.setOptions(layerOptions[i]);
-}
-
-const addLayer = (numStrokes) => {
-  const i = layers.length;
-  layerOptions.push(generateOptions(i));
-  if (devMode) {
-    createLayerGUI(dev.gui, layers, i);
-  }
-  const layer = createLayer(numStrokes);
-  createStrokes(layer, i);
-}
-
-const onClick = (event) => {
-  switch (composition) {
+function onClick(event) {
+  switch (comp) {
     case 'addnew':
       addLayer(strokesPerLayer);
       break;
@@ -345,43 +341,31 @@ const onClick = (event) => {
   }
 }
 
-let numCells = 0;
-const doCreateCell = () => {
-  numCells++;
-  renderToHist();
-  layers.map((layer) => {
-    regenerateLayer(layer);
-  });
-}
-
-let timeoutID = -1;
-const createCell = () => {
-  if (timeoutID > 0) {
-    clearTimeout(timeoutID);
-    doCreateCell();
-  }
-  if (numCells < options.maxCells) {
-    timeoutID = setTimeout(createCell, FXRand.int(500, 7000));
-  }
-  else {
-    // todo: add diagonal animation effect
+function onKeyDown(event) {
+  if (devMode) {
+    dev.keyDown(event, settings, lightOptions, effects);
   }
 }
 
-//scene.add(new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100), new THREE.MeshBasicMaterial({color: new THREE.Color(1, 0, 0)})));
-
-for (let i=0; i<features.layers; i++) {
-  addLayer(strokesPerLayer);
-}
-switch (composition) {
-  case 'cells':
-    createCell();
-    break;
+function onResize(event) {
+  core.resize(window.innerWidth, window.innerHeight);
+  for (let i=0; i<layers.length; i++) {
+    layers[i].resize(window.innerWidth, window.innerHeight, 1);
+  }
 }
 
-core.useEffects(effects);
-core.animate();
+function onDblClick(event) {
+  if (devMode && !dev.isGui(event.target)) {
+    document.location.reload();
+  }
+}
 
-addEventListeners();
-
-fxpreview();
+function addEventListeners() {
+  window.addEventListener('renderFrame', draw);
+  renderer.domElement.addEventListener('click', onClick);
+  document.addEventListener("keydown", onKeyDown, false);
+  window.addEventListener("resize", onResize);
+  if (devMode) {
+    window.addEventListener("dblclick", onDblClick);
+  }
+}
---
+++ src/vars.js
@@ -0,0 +1,37 @@
+import {chooseComposition, choosePalette, options} from "./config";
+import * as FXRand from "../../fxhash_lib/random";
+import {RenderPingPong} from "../../fxhash_lib/RenderPingPong";
+import * as core from "../../fxhash_lib/core";
+import * as THREE from "three";
+import {FluidLayer} from "./FluidLayer";
+import {generateHSLPalette, hsl2Color} from "../../fxhash_lib/color";
+
+let palette, hslPalette, colors, comp, layers, strokesPerLayer, histPingPong, histMesh, labels, features;
+
+const initVars = () => {
+    palette = choosePalette();
+    hslPalette = generateHSLPalette(palette);
+    colors = hslPalette.map(hsl2Color);
+    comp = chooseComposition();
+    layers = [];
+    strokesPerLayer = FXRand.int(options.minStrokes, options.maxStrokes);
+    histPingPong = new RenderPingPong(core.width, core.height, {
+        minFilter: THREE.LinearFilter,
+        magFilter: THREE.LinearFilter,
+        depthBuffer: false,
+        stencilBuffer: false,
+        generateMipmaps: false,
+    });
+    histMesh = FluidLayer.createMesh();
+    labels = new THREE.Group();
+
+    // Feature generation
+    features = {
+        palette: palette,
+        layers: FXRand.int(options.minLayers, options.maxLayers),
+        colorW: FXRand.exp(0.1, 8),
+    }
+    window.$fxhashFeatures = features;
+}
+
+export {initVars, palette, hslPalette, colors, comp, layers, strokesPerLayer, histPingPong, histMesh, labels, features};
